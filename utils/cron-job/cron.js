5ff1f21e8f4df3fac1ffe8d7729cb0408e4955a7
itsmeajay1699 <ajayroy03377@gmail.com>
2024-08-24 15:17:36 +0530

    cron change

--- utils/cron-job/cron.js
+++ utils/cron-job/cron.js
@@ -1,6 +1,6 @@
 import cron from "node-cron";
 
-cron.schedule("* * * * *", async () => {
+cron.schedule("*/5 * * * *", async () => {
   const backendUrl = process.env.BACKEND_URI_PROD;
   console.log("backendUrl", backendUrl);
   const res = await fetch(backendUrl, {
